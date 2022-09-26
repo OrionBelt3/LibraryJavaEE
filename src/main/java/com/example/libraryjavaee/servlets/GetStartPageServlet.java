b6d3572f3eae87b22d393b886b417f7b153ad8d0
Fedor Ivanov <kenola82007@gmail.com>
2022-09-26 19:48:46 +0300

    Add Get and Post method

--- src/main/java/com/example/libraryjavaee/servlets/GetStartPageServlet.java
+++ src/main/java/com/example/libraryjavaee/servlets/GetStartPageServlet.java
@@ -1,16 +1,59 @@
 package com.example.libraryjavaee.servlets;
 
+import com.example.libraryjavaee.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+;
 
 public class GetStartPageServlet extends HttpServlet {
+    private static long COUNT;
+    List<User> users;
+    @Override
+    public void init() throws ServletException {
+        COUNT = 0;
+        users = new CopyOnWriteArrayList<>();
+        users.add(new User(COUNT++, "First", "First", 21));
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String startPage = "/WEB-INF/views/startPage.jsp";
+        req.setAttribute("users", users);
         req.getRequestDispatcher(startPage).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        if (!requestIsValid(req)) {
+            doGet(req, resp);
+        }
+
+        final String name = req.getParameter("name");
+        final String surname = req.getParameter("surname");
+        final String age = req.getParameter("age");
+
+        final User user = new User(COUNT++, name, surname, Integer.parseInt(age));
+        users.add(user);
+
+        doGet(req, resp);
+    }
+
+    private boolean requestIsValid(HttpServletRequest req) {
+        final String name = req.getParameter("name");
+        final String surname = req.getParameter("surname");
+        final String age = req.getParameter("age");
+
+        return name != null && name.length()>0 &&
+                surname != null && surname.length()>0 &&
+                age.matches("[+]?\\d+") && Integer.parseInt(age) > 0;
     }
 }
--- src/main/webapp/WEB-INF/views/startPage.jsp
+++ src/main/webapp/WEB-INF/views/startPage.jsp
@@ -1,12 +1,36 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>Library</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
+<h1><%= "Start page" %></h1>
+<hr />
+<h2><%= "List of users:"%></h2>
+<c:forEach var="user" items="${requestScope.users}">
+    <ul>
+        <li>Имя: <c:out value="${user.name}"/></li>
+        <li>Фамилия: <c:out value="${user.surname}"/></li>
+        <li>Возраст: <c:out value="${user.age}"/></li>
+    </ul>
+    <hr />
+
+</c:forEach>
+
+<h2><%= "Create new user:"%></h2>
+
+<form method="post" action="">
+
+    <label><input type="text" name="name"></label>Имя<br>
+
+    <label><input type="text" name="surname"></label>Фамилия<br>
+
+    <label><input type="number" name="age"></label>Возраст<br>
+
+    <input type="submit" value="Ok" name="Ok"><br>
+</form>
 
 </body>
 </html>
